7dfe45ec9df4d9b3307096bf98510d222e1cb077
kiekiku <hsmny1022@gmail.com>
2022-03-29 15:56:17 +0000

    cartitemコントローラまで作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
+  get 'cart_items/index'
+
+  get 'customers/show'
+
+  get 'customers/edit'
+
+  get 'sessions/new'
+
+  get 'registrations/new'
+
+  get 'items/index'
+
+  get 'items/show'
+
+  get 'homes/top'
+
+  get 'homes/about'
+
+  devise_for :admins
+  devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
